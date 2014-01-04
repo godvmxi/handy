2c32cf3821d5c92f11906c0af4340af99a36eab9
Dandan <godvmxi@gmail.com>
2014-01-04 20:14:43 +0800

    add update file func and set media root

--- handy/settings.py
+++ handy/settings.py
@@ -49,7 +49,11 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/upload/'
+import os
+MEDIA_ROOT =( os.path.dirname(__file__)+'/../upload/' ).replace('\\','/')
+
+print 'media root -> %s'%(MEDIA_ROOT)
+
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
